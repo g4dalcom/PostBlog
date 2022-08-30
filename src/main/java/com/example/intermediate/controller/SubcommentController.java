706ba20c3bb6eb8af1d24c8dafcf7f7954d2d295
seulbin0207 <doverbeach27@gmail.com>
2022-08-30 17:53:45 +0900

    작업 내용 수정

--- src/main/java/com/example/intermediate/controller/SubcommentController.java
+++ src/main/java/com/example/intermediate/controller/SubcommentController.java
@@ -1,4 +1,4 @@
-package com.example.intermediate.controller.response;
+package com.example.intermediate.controller;
 
 import com.example.intermediate.controller.request.SubcommentRequestDto;
 import com.example.intermediate.controller.response.ResponseDto;
